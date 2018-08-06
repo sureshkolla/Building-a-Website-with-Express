5a536936de03ff92ec3832ef3952383cf506894a
Kolla <skolla@Galaxe.com>
2018-08-06 21:12:56 +0530

    assignment 1

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('shop/index', { title: 'Shopping Cart' });
+});
+
+/* GET Cart page. */
+router.get('/cart', function(req, res, next) {
+  res.render('shop/cart', { title: 'Shopping Cart' });
+});
+
+module.exports = router;
